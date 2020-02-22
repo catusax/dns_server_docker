d50ad64e47622a3e6509c556931e07cb9d982ad5
coolrc136 <qq1782536964@live.com>
2020-02-22 23:28:55 +0800

    :sparkles: update to 1.6-rc9

--- config/updatedomian.sh
+++ config/updatedomian.sh
@@ -1,5 +1,2 @@
-wget https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt
-
-cat gfwlist.txt | base64 -d | sort -u | sed '/^$\|@@/d'| sed 's#!.\+##; s#|##g; s#@##g; s#http:\/\/##; s#https:\/\/##;' | sed '/\*/d; /apple\.com/d; /sina\.cn/d; /sina\.com\.cn/d; /baidu\.com/d; /qq\.com/d' | sed '/^[0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+$/d' | grep '^[0-9a-zA-Z\.-]\+$' | grep '\.' | sed 's#^\.\+##' | sort -u > /tmp/temp_gfwlist.txt
-curl https://raw.githubusercontent.com/hq450/fancyss/master/rules/gfwlist.conf | sed 's/ipset=\/\.//g; s/\/gfwlist//g; /^server/d' > /tmp/temp_koolshare.txt
-cat /tmp/temp_gfwlist.txt /tmp/temp_koolshare.txt | sort -u > gfw_all_domain.txt
+wget https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/fullgfwlist.acl
+cat fullgfwlist.acl |grep "(" > domain_alternative_sample
